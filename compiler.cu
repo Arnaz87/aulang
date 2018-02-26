a0681c7af767946b5ca8541300c1ee4df9227132
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-02-26 00:28:21 -0400

    Compiling types, functions, and writing to a file

--- compiler.cu
+++ compiler.cu
@@ -3,10 +3,16 @@ import cobre.system {
   void print (string);
   void quit (int);
   string readall (string);
+
+  type file as file; 
+  file open (string filename, string mode);
+  void write (file, string);
+  void writebyte (file, int);
 }
 
 import cobre.string {
   string itos(int);
+  int length (string) as strlen;
 }
 
 import parser {
@@ -49,7 +55,7 @@ struct Map {
       Pair pair = this.arr[i];
       if (key == pair.key) return pair.id;
     }
-    print("Error: " + key + " not found in map");
+    print("Error: \"" + key + "\" not found in map");
     quit(1);
   }
 
@@ -65,14 +71,16 @@ struct Map {
     }
   }
 
-  void print (Map this) {
+  void print (Map this, string ident) {
     int i = 0;
     while (i < this.pos) {
       Pair pair = this.arr[i];
-      print(pair.key + ": " + itos(pair.id));
+      print(ident + pair.key + ": " + itos(pair.id));
       i = i+1;
     }
   }
+
+  bool any (Map this) { return this.arr.len() > 0; }
 }
 
 Map newMap () { return new Map(emptyPairArr(), 0); }
@@ -113,6 +121,42 @@ struct Module {
 struct Type {
   int module;
   string name;
+  Map getters;
+  Map setters;
+  Map methods;
+  Map casts;
+  int constructor;
+}
+
+Type newType (int module, string name) {
+  return new Type(module, name, newMap(), newMap(), newMap(), newMap(), 0-1);
+}
+
+import cobre.`null` (Node) {
+  type `` as NodeNull {
+    bool isnull ();
+    Node get ();
+  }
+  NodeNull `null` () as nullNode;
+  NodeNull `new` (Node) as newNullNode;
+}
+
+struct Function {
+  int module; // -1 for defined function
+  string name;
+  StrArr outs;
+  StrArr ins;
+  StrArr in_names;
+  NodeNull code;
+
+  bool hasCode (Function this) {
+    if (this.code.isnull()) return 1<0;
+    else return 0<1;
+  }
+}
+
+Function newFunction () {
+  return new Function(0-1, "", emptyStrArr(), emptyStrArr(), emptyStrArr(), nullNode());
 }
 
 import cobre.array (Module) {
@@ -135,27 +179,88 @@ import cobre.array (Type) {
   TypeArr empty () as emptyTypeArr;
 }
 
+import cobre.array (Function) {
+  type `` as FunctionArr {
+    Function get (int);
+    void set (int, Function);
+    int len ();
+    void push (Function);
+  }
+  FunctionArr empty () as emptyFunctionArr;
+}
+
 struct Compiler {
   Node tree;
   Map typeMap;
-  Map funcMap;
+  Map fnMap;
   ModuleArr modules;
   TypeArr types;
-  NodeArr functions;
+  FunctionArr functions;
+  Map tpExports;
+  Map fnExports;
 }
 
 void makeBasics (Compiler c) {
-  c.modules.push(new Module("system", emptyStrArr()));
-  c.modules.push(new Module("int", emptyStrArr()));
-  c.modules.push(new Module("string", emptyStrArr()));
+  c.modules.push(new Module("cobre.core", emptyStrArr()));
+  c.modules.push(new Module("cobre.int", emptyStrArr()));
+  c.modules.push(new Module("cobre.string", emptyStrArr()));
 
-  c.types.push(new Type(1, "bool"));
-  c.types.push(new Type(2, "int"));
-  c.types.push(new Type(3, "string"));
+  c.types.push(newType(2, "bool"));
+  c.types.push(newType(3, "int"));
+  c.types.push(newType(4, "string"));
+  c.types.push(newType(4, "char"));
 
   c.typeMap["bool"] = 0;
   c.typeMap["int"] = 1;
   c.typeMap["string"] = 2;
+  c.typeMap["char"] = 3;
+}
+
+Function, string fnFromNode (Node item) {
+  string alias = item.val;
+  if (alias == "") alias = item.child(2).val;
+  Function f = newFunction();
+  f.name = item.val;
+
+  Node innd = item.child(0);
+  int k = 0;
+  while (k < innd.len()) {
+    Node argnd = innd.child(k);
+    f.ins.push(argnd.child(0).val);
+    f.in_names.push(argnd.child(1).val);
+    k = k+1;
+  }
+
+  Node outnd = item.child(1);
+  int k = 0;
+  while (k < outnd.len()) {
+    f.outs.push(outnd.child(k).val);
+    k = k+1;
+  }
+
+  return f, alias;
+}
+
+void thisArg (Function fn, string arg) {
+  StrArr oldArr = fn.ins;
+  StrArr newArr = emptyStrArr();
+  newArr.push(arg);
+  int i = 0;
+  while (i < oldArr.len()) {
+    newArr.push(oldArr[i]);
+    i = i+1;
+  }
+  fn.ins = newArr;
+
+  StrArr oldArr = fn.in_names;
+  StrArr newArr = emptyStrArr();
+  newArr.push("this");
+  int i = 0;
+  while (i < oldArr.len()) {
+    newArr.push(oldArr[i]);
+    i = i+1;
+  }
+  fn.in_names = newArr;
 }
 
 void makeImports (Compiler c) {
@@ -163,6 +268,8 @@ void makeImports (Compiler c) {
   while (i < c.tree.len()) {
     Node node = c.tree.child(i);
     if (node.tp == "import") {
+      int id = c.modules.len() + 2;
+
       StrArr args = emptyStrArr();
       Node argnd = node.child(0);
       int j = 0;
@@ -172,19 +279,195 @@ void makeImports (Compiler c) {
       }
       c.modules.push(new Module(node.val, args));
 
-      int id = c.modules.len();
 
       int j = 1; // Skip first child, it's the argument node
       while (j < node.len()) {
         Node item = node.child(j);
         if (item.tp == "type") {
           int typeid = c.types.len();
-          string alias = item.child(0).val;
-          c.types.push(new Type(id, item.val));
-          c.typeMap[alias] = typeid;
+          string tp_alias = item.child(0).val;
+          if (tp_alias == "") tp_alias = item.val;
+          Type tp = newType(id, item.val);
+          c.types.push(tp);
+          c.typeMap[tp_alias] = typeid;
+
+          int k = 1;
+          while (k < item.len()) {
+            Node member = item.child(k);
+            if (member.tp == "function") {
+              int fnid = c.functions.len();
+              Function f; string fn_alias;
+              f, fn_alias = fnFromNode(member);
+              f.module = id;
+              thisArg(f, tp_alias);
+              c.functions.push(f);
+              tp.methods[fn_alias] = fnid;
+            } else if (member.tp == "decl") {
+              string name = member.val;
+              string tpnm = member.child(0).val;
+
+              string suffix = "";
+              if (item.val == "") {} else { suffix = ":" + item.val; }
+
+              string getname = name + ":get" + suffix;
+              string setname = name + ":set" + suffix;
+
+              int getid = c.functions.len();
+
+              Function getfn = newFunction();
+              getfn.name = name + ":get" + suffix;
+              getfn.ins.push(tp_alias);
+              getfn.outs.push(tpnm);
+              getfn.module = id;
+
+              Function setfn = newFunction();
+              setfn.name = name + ":set" + suffix;
+              setfn.ins.push(tp_alias);
+              setfn.ins.push(tpnm);
+              setfn.module = id;
+
+              c.functions.push(getfn);
+              c.functions.push(setfn);
+
+              tp.getters[name] = getid;
+              tp.setters[name] = getid+1;
+            } else {
+              print("Unknown type member " + member.tp);
+              quit(1);
+            }
+            k = k+1;
+          }
+        } else if (item.tp == "function") {
+          int fnid = c.functions.len();
+          Function f; string alias;
+          f, alias = fnFromNode(item);
+          f.module = id;
+          c.functions.push(f);
+          c.fnMap[alias] = fnid;
+        } else {
+          print("Unknown imported item: " + item.tp);
+          quit(1);
         }
         j = j+1;
       }
+    }
+    i = i+1;
+  }
+}
+
+void makeTypes (Compiler c) {
+  int i = 0;
+  while (i < c.tree.len()) {
+    Node node = c.tree.child(i);
+    if (node.tp == "type") {
+      int moduleid = c.modules.len() + 2;
+      string base = node.child(0).val;
+      StrArr args = emptyStrArr();
+      args.push(base);
+      c.modules.push(new Module("cobre.typeshell", args));
+
+      int typeid = c.types.len();
+      string alias = node.val;
+      Type tp = newType(moduleid, "");
+      c.types.push(tp);
+      c.typeMap[alias] = typeid;
+      c.tpExports[alias] = typeid;
+
+      int fromid = c.functions.len();
+      Function from = newFunction();
+      from.module = moduleid;
+      from.ins.push(base);
+      from.outs.push(alias);
+      from.name = "new";
+
+      Function to = newFunction();
+      to.module = moduleid;
+      to.ins.push(alias);
+      to.outs.push(base);
+      to.name = "get";
+
+      c.functions.push(from);
+      c.functions.push(to);
+
+      tp.constructor = fromid;
+      tp.casts[base] = fromid + 1;
+    }
+    if (node.tp == "struct") {
+      int moduleid = c.modules.len() + 2;
+      StrArr args = emptyStrArr();
+      c.modules.push(new Module("cobre.record", args));
+
+      int typeid = c.types.len();
+      string alias = node.val;
+      Type tp = newType(moduleid, "");
+      c.types.push(tp);
+      c.typeMap[alias] = typeid;
+      c.tpExports[alias] = typeid;
+
+      int fieldid = 0;
+      int j = 0;
+      while (j < node.len()) {
+        Node member = node.child(j);
+        if (member.tp == "decl") {
+          string name = member.val;
+          string ftp = member.child(0).val;
+
+          args.push(ftp);
+
+          int getid = c.functions.len();
+          Function getter = newFunction();
+          getter.module = moduleid;
+          getter.ins.push(alias);
+          getter.outs.push(ftp);
+          getter.name = "get" + itos(fieldid);
+
+          Function setter = newFunction();
+          setter.module = moduleid;
+          setter.ins.push(alias);
+          setter.ins.push(ftp);
+          setter.name = "set" + itos(fieldid);
+
+          c.functions.push(getter);
+          c.functions.push(setter);
+
+          tp.getters[name] = getid;
+          tp.setters[name] = getid+1;
+
+          c.fnExports[name + ":get:" + alias] = getid;
+          c.fnExports[name + ":set:" + alias] = getid+1;
+
+          fieldid = fieldid + 1;
+        } else if (member.tp == "function") {
+          int fnid = c.functions.len();
+          Function f; string fn_alias;
+          f, fn_alias = fnFromNode(member);
+          f.code = newNullNode(member.child(3));
+          c.functions.push(f);
+          tp.methods[fn_alias] = fnid;
+          c.fnExports[fn_alias + ":" + alias] = fnid;
+        } else {
+          print("Unknown struct member " + member.tp);
+          quit(1);
+        }
+        j = j+1;
+      }
+    }
+    i = i+1;
+  }
+}
+
+void makeFunctions (Compiler c) {
+  int i = 0;
+  while (i < c.tree.len()) {
+    Node node = c.tree.child(i);
+    if (node.tp == "function") {
+      int id = c.functions.len();
+      Function f; string name;
+      f, name = fnFromNode(node);
+      f.code = newNullNode(node.child(3));
+      c.functions.push(f);
+      c.fnMap[name] = id;
+      c.fnExports[name] = id;
     }
     i = i+1;
   }
@@ -199,25 +482,25 @@ Compiler compile (string src) {
     newMap(),
     emptyModuleArr(),
     emptyTypeArr(),
-    emptyNodeArr()
+    emptyFunctionArr(),
+    newMap(),
+    newMap()
   );
 
   makeBasics(c);
-
   makeImports(c);
+  makeTypes(c);
+  makeFunctions(c);
 
   return c;
 }
 
-
-void main () {
-  Compiler c = compile("import cobre.lol { void print(string); type foo as fox; type bar as bax {int x;} }");
-
+void printCompiler (Compiler c) {
   print("Type Map:");
-  c.typeMap.print();
+  c.typeMap.print("");
 
   print("\nFunction Map:");
-  c.funcMap.print();
+  c.fnMap.print("");
 
   print("\nModules:");
   int i = 0;
@@ -229,7 +512,7 @@ void main () {
       args = args + m.args[j] + " ";
       j = j+1;
     }
-    print("[" + itos(i+1) + "]: " + m.name + args + ")");
+    print("[" + itos(i+2) + "]: " + m.name + args + ")");
     i = i+1;
   }
 
@@ -238,9 +521,247 @@ void main () {
   while (i < c.types.len()) {
     Type t = c.types[i];
     print("[" + itos(i) + "]: Module[" + itos(t.module) + "]." + t.name);
+    if (t.constructor < 0) {} else {
+      print("  new: " + itos(t.constructor));
+    }
+    if (t.getters.any()) {
+      print("  getters:");
+      t.getters.print("    ");
+    }
+    if (t.setters.any()) {
+      print("  setters:");
+      t.setters.print("    ");
+    }
+    if (t.methods.any()) {
+      print("  methods:");
+      t.methods.print("    ");
+    }
+    if (t.casts.any()) {
+      print("  casts to:");
+      t.casts.print("    ");
+    }
     i = i+1;
   }
 
   print("\nFunctions:");
-  printNodeArr(c.functions);
+  int i = 0;
+  while (i < c.functions.len()) {
+    Function f = c.functions[i];
+
+    string args = " ( ";
+    int j = 0;
+    while (j < f.ins.len()) {
+      args = args + f.ins[j] + " ";
+      j = j+1;
+    }
+    args = args + ") -> ( ";
+    int j = 0;
+    while (j < f.outs.len()) {
+      args = args + f.outs[j] + " ";
+      j = j+1;
+    }
+    args = args + ")";
+
+
+    if (f.hasCode()) {
+      print("[" + itos(i) + "]: <Code>" + args);
+    } else {
+      print("[" + itos(i) + "]: Module[" + itos(f.module) + "]." + f.name + args);
+    }
+    i = i+1;
+  }
+
+  print("\nType exports:");
+  c.tpExports.print("");
+
+  print("\nFunction exports:");
+  c.fnExports.print("");
+}
+
+// =============================== //
+//           File Writing          //
+// =============================== //
+
+void writenum (file f, int n) {
+  // TODO: correctly format n > 127
+  writebyte(f, n);
+}
+
+void writestr (file f, string s) {
+  writenum(f, strlen(s));
+  write(f, s);
+}
+
+void writeExports (Compiler c, file f) {
+  int exportcount = c.fnExports.arr.len() + c.tpExports.arr.len();
+  writebyte(f, 1); // Export module, kind 1 is defined
+  writebyte(f, exportcount);
+  int i = 0;
+  while (i < c.tpExports.arr.len()) {
+    Pair p = c.tpExports.arr[i];
+    writebyte(f, 1); // Item kind 1 is type
+    writenum(f, p.id);
+    writestr(f, p.key);
+    i = i+1;
+  }
+
+  int i = 0;
+  while (i < c.fnExports.arr.len()) {
+    Pair p = c.fnExports.arr[i];
+    writebyte(f, 2); // Item kind 2 is function
+    writenum(f, p.id);
+    writestr(f, p.key);
+    i = i+1;
+  }
+}
+
+void writeModules (Compiler c, file f) {
+  int count = c.modules.len() + 1;
+
+  int argcount = 0; // Number of modules with arguments
+  int i = 0;
+  while (i < c.modules.len()) {
+    if (c.modules[i].args.len() > 0)
+      argcount = argcount + 1;
+    i = i+1;
+  }
+
+  writenum(f, count + argcount + argcount); // For now, omit argument modules
+
+  writeExports(c, f);
+
+  // This one is an index, so it does count this module's argument
+  int argi = count+1;
+  int i = 0;
+  while (i < c.modules.len()) {
+    Module m = c.modules[i];
+    if (m.args.len() > 0) {
+      // Every module with argumetns consists of three modules
+      // the imported functor, the argument module
+      // and the applied functor.
+      // This one is the last
+      writebyte(f, 4); // Kind 4 is build functor
+      writenum(f, argi); // The functor
+      writenum(f, argi+1); // The argument
+      argi = argi+2;
+    } else {
+      writebyte(f, 0); // Kind 0 is import
+      writestr(f, m.name);
+    }
+    i = i+1;
+  }
+
+  // Now the two parts of a functor module
+  int i = 0;
+  while (i < c.modules.len()) {
+    Module m = c.modules[i];
+    if (m.args.len() > 0) {
+      writebyte(f, 2); // Kind 2 is import functor
+      writestr(f, m.name);
+
+      writebyte(f, 1); // Kind 1 is defined module
+      writebyte(f, m.args.len()); // With 0 items
+      int j = 0;
+      while (j < m.args.len()) {
+        string tpname = m.args[j];
+        int tpid = c.typeMap[tpname];
+        writebyte(f, 1); // Item kind 1 is type
+        writenum(f, tpid);
+        writestr(f, itos(j));
+        j = j+1;
+      }
+    }
+    i = i+1;
+  }
+}
+
+void writeFunctions (Compiler c, file f) {
+  writenum(f, c.functions.len());
+
+  int i = 0;
+  while (i < c.functions.len()) {
+    Function fn = c.functions[i];
+
+    if (fn.hasCode()) {
+      writebyte(f, 2); // Kind 2 is function with code
+    } else {
+      writebyte(f, 1); // Kind 1 is imported function
+      writenum(f, fn.module);
+      writestr(f, fn.name);
+    }
+
+    writenum(f, fn.ins.len());
+    int j = 0;
+    while (j < fn.ins.len()) {
+      string tpname = fn.ins[j];
+      int tpid = c.typeMap[tpname];
+      writenum(f, tpid);
+      j = j+1;
+    }
+
+    writenum(f, fn.outs.len());
+    int j = 0;
+    while (j < fn.outs.len()) {
+      string tpname = fn.outs[j];
+      int tpid = c.typeMap[tpname];
+      writenum(f, tpid);
+      j = j+1;
+    }
+
+    i = i+1;
+  }
+}
+
+void writeBodies (Compiler c, file f) {
+  int i = 0;
+  while (i < c.functions.len()) {
+    Function fn = c.functions[i];
+
+    if (fn.hasCode()) {
+      // TODO: Implement actual bodies
+      // For now, all bodies are an empty return...
+      writenum(f, 1);
+      writenum(f, 0);
+    }
+
+    i = i+1;
+  }
+}
+
+void writeCompiler (Compiler c, string filename) {
+  file f = open(filename, "w");
+  write(f, "Cobre ~4");
+  writebyte(f, 0); // end signature
+
+  writeModules(c, f);
+
+  writenum(f, c.types.len());
+  int i = 0;
+  while (i < c.types.len()) {
+    Type tp = c.types[i];
+    writebyte(f, 1); // Kind 1 is import
+    writenum(f, tp.module);
+    writestr(f, tp.name);
+    i = i+1;
+  }
+
+  writeFunctions(c, f);
+
+  writebyte(f, 0); // constants
+
+  writeBodies(c, f);
+
+  writebyte(f, 1); // static function
+  writebyte(f, 0); //   (just return)
+
+  writebyte(f, 0); // metadata
+}
+
+
+void main () {
+  string src = readall("test.cu");
+  Compiler c = compile(src);
+
+  //printCompiler(c);
+  writeCompiler(c, "out");
 }
--- test.cu
+++ test.cu
@@ -1,2 +1,45 @@
 
-type NodeShell (Node);
+import cobre.system {
+  void print (string);
+}
+
+import cobre.string {
+  string itos(int);
+}
+
+void main () {
+  int bottles = 5;
+
+  while (bottles > 0) {
+    string plural = " bottles";
+    if (bottles == 1) { plural = " bottle"; }
+
+    string bottle_s = itos(bottles);
+
+    print(bottle_s + plural + " of beer on the wall");
+    print(bottle_s + plural + " of beer");
+    print("Take one down, pass it around");
+    bottles = bottles - 1;
+    
+    if (bottles == 1) { plural = " bottle"; }
+    else { plural = " bottles"; }
+    
+    print(itos(bottles) + plural + " of beer on the wall");
+    print("");
+  }
+}
+
+struct Pair {
+  string key;
+  int id;
+}
+
+import cobre.array (Pair) {
+  type `` as PairArr {
+    Pair get (int);
+    void set (int, Pair);
+    int len ();
+    void push (Pair);
+  }
+  PairArr empty () as emptyPairArr;
+}
